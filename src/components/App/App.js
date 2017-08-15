bfaacb8db51276d1ecc344d467719190d78ae4b4
Etay Barzilay <etaybarzilay89@gmail.com>
2017-08-15 16:02:19 +0300

    initial commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,19 +4,21 @@ import s from './App.scss';
 import Row from '../Row';
 import Header from '../Header';
 import FormulaEditor from '../FormulaEditor';
+import DevTools from 'mobx-react-devtools';
 
 function App() {
-    return (
-        <div className={s.root}>
-            <FormulaEditor />
-            <table className={s.table}>
-                <tbody>
-                    <Header/>
-                    {_.times(10, rowIndex => <Row key={rowIndex} rowIndex={rowIndex}/>)}
-                </tbody>
-            </table>
-        </div>
-    );
+  return (
+    <div className={s.root}>
+      <FormulaEditor/>
+      <table className={s.table}>
+        <tbody>
+          <Header/>
+          {_.times(10, rowIndex => <Row key={rowIndex} rowIndex={rowIndex}/>)}
+        </tbody>
+      </table>
+      <DevTools/>
+    </div>
+  );
 }
 
 export default App;
