56a9570ede0bdc71005408ad503c350054fe43dd
Etay Barzilay <etaybarzilay89@gmail.com>
2017-08-16 19:39:20 +0300

    refactoring

--- src/components/Store/Store.js
+++ src/components/Store/Store.js
@@ -6,7 +6,7 @@ const Store = observable({
   sheet: observable.map({}),
   selectedCell: '',
   getCellFormula(row, column) {
-    return this.sheet.get(this.cellIndexToString(row, column));
+    return this.sheet.get(this.cellIndexToString(row, column)) || '';
   },
   cellIndexToString(row, column) {
     return row + '_' + column;
@@ -26,8 +26,17 @@ const Store = observable({
     this.setCellFormula(cellIndexArray[0], cellIndexArray[1], formula);
   },
   computeValue(formula) {
-    const computedFormula = parseFormula(formula);
-    return computedFormula;
+    const cellRegex = new RegExp('([A-J][1-9][0-9]*)', 'g');
+    const replacedFormula = formula.replace(cellRegex, (x, match) => {
+      return this.computeValue(this.cellStringToVal(match));
+    });
+
+    return eval(replacedFormula || formula);
+  },
+  cellStringToVal(cellString) {
+    const letterToNumber = cellString[0].charCodeAt(0) - 65;
+    const number = parseInt(cellString.substr(1));
+    return Store.getCellFormula(number - 1, letterToNumber);
   },
   setSelectedCell: mobx.action(function (row, column) {
     this.selectedCell = this.cellIndexToString(row, column);
@@ -37,32 +46,6 @@ const Store = observable({
     this.sheet.set(cellIndexString, '' + formula);
   }),
 });
-
-const parseFormula = formula => {
-  const stringformula = '' + formula;
-  if (!stringformula) {
-    return '';
-  } else if (stringformula.length === 1) {
-    return eval(stringformula);
-  }
-
-  const cellRegex = new RegExp('([A-J][1-9][0-9]*)', 'g');
-
-  const replacedFormula = stringformula.replace(cellRegex, (x, match) => {
-    return parseFormula(cellStringToVal(match));
-  });
-
-  return eval(replacedFormula);
-};
-
-const cellStringToVal = cellString => {
-  const letter = cellString[0];
-  const number = parseInt(cellString.substr(1));
-  const arrayIndexFromLetter = letter.charCodeAt(0) - 65;
-  return Store.getCellFormula(number - 1, arrayIndexFromLetter);
-};
-
-
 
 window.Store = Store;
 export default Store;
